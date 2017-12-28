acb53e83a8b1af76656bc769a6395bbd37df3374
samo <samek@me.com>
2017-12-28 14:22:45 +0100

    added libs and --enable-gpl

--- compile.sh
+++ compile.sh
@@ -9,12 +9,12 @@
 #ln -sf /usr/bin/g++-6 /usr/bin/g++
 #ln -sf /usr/bin/gcc-6 /usr/bin/gcc
 
+#install libs
+
+cd /tmp
+
 git clone https://github.com/twitter/vireo.git
 git clone https://github.com/l-smash/l-smash.git
 
-cd l-smash && git fetch --all --tags --prune && git checkout tags/v2.9.1 && ./configure --enable-shared && make && make install
-cd ../vireo/vireo
-export PREFIX=/usr/local/
-./configure --prefix=$PREFIX
-make
-make install
+cd l-smash && git fetch --all --tags --prune && git checkout tags/v2.9.1 && ./configure --enable-shared && make && make install && ldconfig \
+&& cd ../vireo/vireo && export PREFIX=/usr/local/ && ./configure --prefix=$PREFIX --enable-gpl && make && make install
